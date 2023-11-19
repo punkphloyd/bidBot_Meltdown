343b0fb5d117ff04be0d9c3ced6df996d4f54fca
punkphloyd <96701180+punkphloyd@users.noreply.github.com>
2023-11-19 00:30:01 +0000

    Finalised scheduled bid application function, working from bid datafile

--- cogs/Scheduler.py
+++ cogs/Scheduler.py
@@ -27,9 +27,11 @@ class Scheduler(commands.Cog):
         self.generate_bid_close.start()
         if debug_mode:
             print(f"{bid_application_time} is the bid application time")
-            print(f"{bid_close_gener_time} is the bid close time")
+            print(f"{bid_close_gener_time} is the bid close generation time")
 
     def cog_unload(self):
+        if debug_mode:
+            print(f"Bid scheduler unloaded at {datetime.now()}")
         self.apply_bids.cancel()
         self.generate_bid_close.cancel()
 
@@ -39,12 +41,12 @@ class Scheduler(commands.Cog):
             print(f"It is {bid_close_gener_time}, the program will now generate a bid close time (selected randomly between 4pm and 7pm)")
 
         # Check if data directory exists, it not then create it
-        data_dir = "./data/"
         data_dir_exists = os.path.exists(data_dir)
         if not data_dir_exists:
             os.makedirs(data_dir)
 
         # File to store random bid window close time, open as 'w' as we always want to overwrite previous content if any present
+        # Only content in this file should always be one single timestamp between 1600 and 1900
         bid_close_file = data_dir + "bid_close.tme"
         bc_file = open(bid_close_file, 'w')
 
@@ -52,11 +54,20 @@ class Scheduler(commands.Cog):
         random_hour = int(random_time/60)
         random_min = (random_time - 60*random_hour)
 
-        bid_close_hour = 16 + random_hour
-        bid_close_minute = 0 + random_min
 
-        print(f"{bid_close_hour}{bid_close_minute}", file=bc_file)
+        bid_close_hour = 16 + int(random_hour)
+        bid_close_min = 0 + int(random_min)
+        # Need this segment to prevent 3 digit timestamps from occurring for minutes returned with single digits
+        if random_min < 10:
+            zero = "0"
+        else:
+            zero = ""
 
+        print(f"{bid_close_hour}{zero}{bid_close_min}", file=bc_file)  # Write bid close timestamp to file
+
+    # Function to apply bids at appropriate time
+    # This function reads through the recorded bids (stored in text file and populated via the /bid /bid2 functions of the main
+    # bot logic); checks them against bid window close timestamps, and then populates the active bids onto the spreadsheet accordingly
     @tasks.loop(time=bid_application_time)
     async def apply_bids(self):
         # Generate appropriate log filename first
@@ -78,7 +89,7 @@ class Scheduler(commands.Cog):
                 print(f"Data directory {data_dir} did not exist, creating now")
             os.makedirs(data_dir)
 
-        # Check bit deadline file exists, otherwise return an error
+        # Check bid deadline file exists, otherwise return an error
         bid_close_file = data_dir + "bid_close.tme"
         if os.path.exists(bid_close_file):
             bcf = open(bid_close_file, 'r')
@@ -90,6 +101,10 @@ class Scheduler(commands.Cog):
             print(f"Error - unable to find bid deadline file {bid_close_file}", file=open(log_filename, 'a'))
             return
         # Read contents of bid close file (should be single line containing a single timestamp)
+        numlines = len(bcf.readlines())
+        if numlines != 1:
+            if debug_mode:
+                print(f"Too many lines in bid close file: {numlines} lines in {bcf}")
         for line in bcf:
             if debug_mode:
                 print(line)
@@ -118,7 +133,6 @@ class Scheduler(commands.Cog):
         # Cycle through bids data file and check if bid is good or bad
         # If bid is good, apply it to google sheets and move to next bid
         # If bid is bad, respond accordingly
-
         for line in bdf:
             if debug_mode:
                 print(line)
@@ -129,21 +143,114 @@ class Scheduler(commands.Cog):
                     print(f"Bid: {bid} should be 6 items in length. It is {len(bid)}")
                 print(f"Bid: {bid} should be 6 items in length. It is {len(bid)}", file=open(log_filename, 'a'))
                 continue
-            # Check if bid is good using player, item, and points values
-            bid_success, message_out = check_bid(bid[3],bid[4],bid[5])       
-            if bid_success:
-                await channel.send(f"Successful bid: {bid[3]} has bid {bid[5]} points on {bid[4]}")
-                # Print success to log file
-                print(f"{bid[2]} - Bid success: {bid_success} \n Message out: {message_out}", file=open(log_filename, 'a'))
 
-                # Copy logic from previous (non time-delay) routine for writing to sheets
-                # Get all existing bids on the corresponding item, and check if player already has an existing bid in place
-                all_bids = get_all_bids(bid[4])
+            player = bid[3]
+            item = bid[4]
+            points = bid[5]
+            date = int(bid[1])
+            month = int(bid[0])
+            bid_time = bid[2]
+            bid_time_hour = int(bid[2][:2])
+            bid_time_min = int(bid[2][-2:])
+
+            # Check if bid was prior to bid window closing
+            # Get date month and time
+            date_now = datetime.now().strftime("%d")
+            month_now = datetime.now().month
+            # Get data for bid close time in appropriate format
+            hour_close = int(bid_ct[:2])
+            min_close = int(bid_ct[-2:])
+
+            # Check if bid was input prior to close of window
+            # If bid was input in previous month, (either new month > old month, or new month == 1), then bid is good
+            if month < month_now or (month_now == 1 and month == 12):
+                bid_good = True
+            else:
+                # If same month, check date
+                # If date is from at least 1 day prior, bid is good
+                if date < int(date_now):
+                    bid_good = True
+                else:
+                    # If bid is from same date, check timestamp again bid close timestamp
+                    # If bid close hour is larger than bid application hour, bid is good
+                    # Or if hours are equal, then bid close minute must exceed bid application minute
+                    if bid_time_hour < hour_close or (bid_time_hour == hour_close and bid_time_min < min_close):
+                        bid_good = True
+                    else:
+                        # If all previous tests have failed, bid has not been submitted prior to passage of
+                        # at least one bid close window
+                        # Therefore bid is not yet good and must remain on the stack
+                        bid_good = False
+
+            # Handle good/bad bids appropriately
+            # Good bids are applied and removed from datafile
+            # Bad bids are not pushed to sheet, remain on datafile for next day's application window(2350)
+
+            if not bid_good:
                 if debug_mode:
-                    print("Original bids: ")
-                    print(all_bids)
-                if bid[3] in all_bids.keys():
-                    pre_bid = all_bids[bid[3]]
+                    print(f"Bid not yet implemented, bid time: {bid_time}, bid window closed: {bid_ct} (Player: {player}, Item: {item}, Points: {points} ")
+                return
+            else:
+                # Check if bid is good using player, item, and points values
+                bid_success, message_out = check_bid(player, item, points)
+                if bid_success:
+                    await channel.send(f"Successful bid: {player} has bid {points} points on {item} as a pending bid")
+                    # Print success to log file
+                    print(f"{date} - Bid success: {bid_success} \n Message out: {message_out}", file=open(log_filename, 'a'))
+
+                    # Copy logic from previous (non time-delay) routine for writing to sheets
+                    # Get all existing bids on the corresponding item, and check if player already has an existing bid in place
+                    # E.g., Fortitude Torque : Hammer bids 10, Shamrock	bids 7, and	Tasco bids 1
+                    # This produces a dictionary which looks like:
+                    # { 'Hammer': 10, 'Shamrock': 7, 'Tasco': 1 }
+                    all_bids = get_all_bids(item)
+                    if debug_mode:
+                        print("Original bids: ")
+                        print(all_bids)
+
+                    pre_bid = get_player_bid(player, item)
+
+                    if pre_bid != 0:  # i.e., player has points already in this item
+                        new_points = int(points) + int(pre_bid)
+                        all_bids[player] = new_points
+                        if debug_mode:
+                            print("Existing bids updated")
+                            print(all_bids)
+                    else:  # pre_points == 0 -> i.e. fresh bid on this item for this player
+                        # Fresh bid - add new bid to this dictionary
+                        all_bids[player] = points
+                        if debug_mode:
+                            print("New bid added:")
+                            print(all_bids)
+
+                    # bid_conv function to convert all points values to integers (code reads as strings otherwise)
+                    all_bids = bid_conv(all_bids)
+                    # Sort all bids by points
+                    all_bids = bid_sort(all_bids)
+
+                    if debug_mode:
+                        # Printing out in debug mode to ensure that column and row being obtained is the one expected
+                        print("column/row for bid_item: ")
+                        col, row = find_cell(item)
+                        print(f"{col}{row}")
+
+                        print("column/row for player: ")
+                        colp, rowp = find_cell(player)
+                        print(f"{colp}{rowp}")
+
+                    if debug_mode:
+                        print(f"Implementing new bids onto {item}")
+                    if debug_mode:
+                        print("Sorted bids: ")
+                        print(all_bids)
+                    print(f"{date}/{month} {bid_time} - Adding bids: {all_bids} onto {item}", file=open(log_filename, 'a'))
+                    update_bids(item, all_bids)
+                # Otherwise, report to the player that the bid has failed and identify the diagnosed cause
+                else:
+                    await channel.send(f"The attempt for {player} to bid {points} points on item {item} was unsuccessful\n {message_out}")
+                    # Print failure to log file
+                    print(f"{bid_time} - Bid success: {bid_success} \n Message out: {message_out}", file=open(log_filename, 'a'))
+
 
 # Function to run the cog within the bot
 def setup(bot):
